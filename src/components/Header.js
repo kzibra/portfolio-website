45b1d36f547eb23a7c34322681a2776cc4ab6725
kzibra <iamzeev0904@gmail.com>
2025-01-21 13:00:00 +0500

    Made accessibility improvements and other updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,18 @@
-import Logo from '../assets/logo.svg'
+import Logo from "../assets/logo.svg";
 
 const Header = () => {
   return (
     <header className="py-8">
-      <div className='container mx-auto'>
-        <div className='flex justify-between items-center'>
-          <a href="#">
+      <div className="container mx-auto">
+        <div className="flex justify-between items-center">
+          <button onClick={() => (window.location.href = "#")}>
             <img src={Logo} alt="" />
-          </a>
-          <button className='btn btn-sm'>Work with me</button>
+          </button>
+          <button className="btn btn-sm">Work with me</button>
         </div>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/components/Services.js
+++ src/components/Services.js
@@ -6,51 +6,52 @@ const services = [
   {
     name: "UI/UX Design",
     description:
-      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maiores, quia quo expedita accusamus illum ducimus.',
-    link: 'Learn more',
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maiores, quia quo expedita accusamus illum ducimus.",
+    link: "Learn more",
   },
   {
     name: "Development",
     description:
-      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maiores, quia quo expedita accusamus illum ducimus.',
-    link: 'Learn more',
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maiores, quia quo expedita accusamus illum ducimus.",
+    link: "Learn more",
   },
   {
     name: "Digital Marketing",
     description:
-      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maiores, quia quo expedita accusamus illum ducimus.',
-    link: 'Learn more',
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maiores, quia quo expedita accusamus illum ducimus.",
+    link: "Learn more",
   },
   {
     name: "Product Branding",
     description:
-      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maiores, quia quo expedita accusamus illum ducimus.',
-    link: 'Learn more',
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maiores, quia quo expedita accusamus illum ducimus.",
+    link: "Learn more",
   },
-]
+];
 
 const Services = () => {
   return (
     <section className="section" id="services">
       <div className="container mx-auto">
         <div className="flex flex-col lg:flex-row">
-          <motion.div 
-            variants={fadeIn('right', 0.3)} 
+          <motion.div
+            variants={fadeIn("right", 0.3)}
             initial="hidden"
-            whileInView={'show'}
+            whileInView={"show"}
             viewport={{ once: false, amount: 0.3 }}
             className="flex-1 lg:bg-services lg:bg-bottom bg-no-repeat mix-blend-lighten mb-12 lg:mb-0"
           >
             <h2 className="h2 text-accent mb-6">What I Do.</h2>
             <h3 className="h3 max-w-[455px] mb-16">
-              I'm a Freelance Front-end Developer with over 5 years of experience.
+              I'm a Freelance Front-end Developer with over 5 years of
+              experience.
             </h3>
             <button className="btn btn-sm">See my work</button>
           </motion.div>
-          <motion.div 
-            variants={fadeIn('left', 0.5)} 
+          <motion.div
+            variants={fadeIn("left", 0.5)}
             initial="hidden"
-            whileInView={'show'}
+            whileInView={"show"}
             viewport={{ once: false, amount: 0.3 }}
             className="flex-1"
           >
@@ -58,8 +59,8 @@ const Services = () => {
               {services.map((service, index) => {
                 const { name, description, link } = service;
                 return (
-                  <div 
-                    className="border-b border-white/20 h-[146px] mb-[38px] flex" 
+                  <div
+                    className="border-b border-white/20 h-[146px] mb-[38px] flex"
                     key={index}
                   >
                     <div className="max-w-[476px]">
@@ -71,22 +72,28 @@ const Services = () => {
                       </p>
                     </div>
                     <div className="flex flex-col flex-1 items-end">
-                      <a href="#" className="btn w-9 h-9 mb-[42px] flex justify-center items-center">
+                      <button
+                        className="btn w-9 h-9 mb-[42px] flex justify-center items-center"
+                        onClick={() => (window.location.href = "#")}
+                      >
                         <BsArrowUpRight />
-                      </a>
-                      <a href="#" className="text-gradient text-sm">
+                      </button>
+                      <button
+                        className="text-gradient text-sm"
+                        onClick={() => (window.location.href = "#")}
+                      >
                         {link}
-                      </a>
+                      </button>
                     </div>
                   </div>
-                )
+                );
               })}
             </div>
           </motion.div>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Services
+export default Services;
